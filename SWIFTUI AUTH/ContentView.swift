4f61f654201374f0889105199fb43c748b0cbe77
aryamanpuri <aryamanpuri@gmail.com>
2021-07-30 21:32:34 +0530

    UI UPDATE

--- SWIFTUI AUTH/ContentView.swift
+++ SWIFTUI AUTH/ContentView.swift
@@ -6,13 +6,101 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+
+class AppViewModel : ObservableObject {
+    
+    let auth = Auth.auth()
+    
+    @Published var signedIn = false
+    var isSignedIn : Bool {
+        return auth.currentUser != nil
+    }
+    
+    func signIn(email : String , password : String) {
+        auth.signIn(withEmail: email, password: password )
+        { [weak self] result, error  in
+            guard result != nil , error == nil else
+            {
+                return
+            }
+            DispatchQueue.main.async {
+                self?.signedIn = true
+            }
+            
+        }
+    }
+    
+    
+    func signUp(email : String , password : String) {
+        auth.createUser(withEmail: email, password: password)
+        { [weak self] result ,error in
+            guard result != nil , error == nil else {
+                return
+            }
+        
+        DispatchQueue.main.async {
+            self?.signedIn = true
+        }
+        }
+    }
+    
+    
+    func signOut(){
+        try? auth.signOut()
+        
+        self.signedIn = false
+    }
+    
+}
 
 struct ContentView: View {
-    @State var email = ""
-    @State var password = ""
+    
+    @EnvironmentObject var viewModel : AppViewModel
     
     var body: some View {
         NavigationView{
+            if viewModel.signedIn{
+                VStack{
+                    Text("You are signed in ")
+                        .padding()
+                    Button(action: {
+                        viewModel.signOut()
+                    }, label: {
+                        Text("SIGN OUT")
+                            .frame(width: 200, height: 50)
+                            .background(Color.green)
+                            .foregroundColor(Color.blue)
+                    })
+                        .padding()
+                }
+            }
+            else{
+            SignInView()
+            }
+    }
+        .onAppear {
+            viewModel.signedIn = viewModel.isSignedIn
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
+
+
+struct SignInView: View {
+    @State var email = ""
+    @State var password = ""
+    
+    @EnvironmentObject var viewModel : AppViewModel
+    
+    var body: some View {
+        
             VStack{
                 Image("logo")
                     .resizable()
@@ -22,17 +110,85 @@ struct ContentView: View {
                 
                 VStack{
                     TextField("Enter Email Address",text: $email)
+                        .disableAutocorrection(true)
+                        .autocapitalization(.none)
                         .padding()
                         .background(Color(.secondarySystemBackground))
                     SecureField("ENTER PASSWORD" , text: $password)
+                        .disableAutocorrection(true)
+                        .autocapitalization(.none)
                         .padding()
                         .background(Color(.secondarySystemBackground))
                     
                     
-                    Button(action: {}
+                    Button(action: {
+                        
+                        guard !email.isEmpty, !password.isEmpty else {
+                            return
+                        }
+                        viewModel.signIn(email: email, password: password)
+                    }
                            ,
                            label: {
                         Text("SIGN IN")
+                            .foregroundColor(Color.white)
+                            .frame(width: 200, height: 50)
+                            .cornerRadius(8)
+                            .background(Color.blue)
+                    })
+                        .padding()
+                    
+                    NavigationLink("Create Account", destination: SignUpView())
+                        .padding()
+                }
+                .padding()
+                
+                Spacer()
+            }
+            .navigationTitle("WELCOME")
+    }
+    }
+
+
+
+struct SignUpView: View {
+    @State var email = ""
+    @State var password = ""
+    
+    @EnvironmentObject var viewModel : AppViewModel
+    
+    var body: some View {
+        
+            VStack{
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200, alignment: .top)
+                
+                
+                VStack{
+                    TextField("Enter Email Address",text: $email)
+                        .disableAutocorrection(true)
+                        .autocapitalization(.none)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                    SecureField("ENTER PASSWORD" , text: $password)
+                        .disableAutocorrection(true)
+                        .autocapitalization(.none)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                    
+                    
+                    Button(action: {
+                        
+                        guard !email.isEmpty, !password.isEmpty else {
+                            return
+                        }
+                        viewModel.signUp(email: email, password: password)
+                    }
+                           ,
+                           label: {
+                        Text("Create Account")
                             .foregroundColor(Color.white)
                             .frame(width: 200, height: 50)
                             .cornerRadius(8)
@@ -43,14 +199,7 @@ struct ContentView: View {
                 
                 Spacer()
             }
-            .navigationTitle("SIGN IN SIGN OUT")
+            .navigationTitle("Create Account")
     }
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .preferredColorScheme(.dark)
-    }
-}
